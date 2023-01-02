f696cf239e9a5f18fdb5ccbbc2bfb767f5b71fc4
5tupidmuffin <rameshwark9500@gmail.com>
2023-01-03 01:21:53 +0530

    handled pygame missing case

--- src/kuzuki/__main__.py
+++ src/kuzuki/__main__.py
@@ -1,13 +1,26 @@
 try:
     from .start_game import start_game
-except ImportError:
-    print(
+except ImportError as exc:
+    if isinstance(exc, ModuleNotFoundError):
+        # dependencies are missing
+        print(
+            """
+            please install dependensies first with,
+            `pip3 install -r requirements.txt`
+            """
+        )
+        exit(1)
+
+    if isinstance(exc, ImportError):
+        # ran it like a script
+        print(
+            """
+            please run this as a MODULE NOT as a script,
+            with `python3 -m src.kuzuki`
         """
-        please run this as a MODULE NOT as a script,
-        with `python -m src.kuzuki`
-    """
-    )
-    exit(1)
+        )
+        exit(1)
+    raise exc
 
 
 start_game()
